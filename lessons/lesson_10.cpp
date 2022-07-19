285636ea8a07d3346eb45d64b81904299e1b2afe
HAWAINA <ismettrabzon08@gmail.com>
2022-07-19 23:12:11 +0600

    cpp_life

---
+++ lessons/lesson_10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct Auto {
+    int wheels;
+    float speed;
+    char color;
+};
+
+int main(int argc, const char * argv[]) {
+
+    Auto shkoda;
+    shkoda.color = 'r';
+
+    
+    cin.get();
+    return 0;
+}
