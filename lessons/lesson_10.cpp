cc3d6e729e690f20fb119cff8faf6e7337bd3794
HAWAINA <ismettrabzon08@gmail.com>
2022-07-22 17:45:45 +0600

    ccp_life

--- lessons/lesson_10.cpp
+++ lessons/lesson_10.cpp
@@ -2,19 +2,33 @@
 
 using namespace std;
 
+struct Date
+{
+    int year;
+};
+
+
 
 struct Auto {
     int wheels;
     float speed;
     char color;
+
+    Date since;
 };
 
 int main(int argc, const char * argv[]) {
 
     Auto shkoda;
     shkoda.color = 'r';
-
+    shkoda.wheels = 4;
+    shkoda.speed = 315.23;
+    shkoda.since.year = 1999;
     
+    Auto audi = {4, 300.23, 'b'};
+
+    cout << "Audi speed: " << audi.speed << "\n" << "Shkoda speed: " << shkoda.speed << endl;
+
     cin.get();
     return 0;
 }
